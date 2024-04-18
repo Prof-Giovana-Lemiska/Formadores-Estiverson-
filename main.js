b36305e6517e1cf37b1828445404d50937f51068
Prof-Giovana-Lemiska <130910854+Prof-Giovana-Lemiska@users.noreply.github.com>
2024-04-18 16:07:34 -0300

    j

--- main.js
+++ main.js
@@ -13,12 +13,6 @@ for(let i=0; i<botoes.length; i++){
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-25T00:00:00");
-let tempoAtual = new Date();
-
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-
-const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 

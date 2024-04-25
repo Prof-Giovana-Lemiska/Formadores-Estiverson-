23c05db51530df24192a6dbabba71837c10908da
Prof-Giovana-Lemiska <130910854+Prof-Giovana-Lemiska@users.noreply.github.com>
2024-04-25 16:40:39 -0300

    c

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for(let i=0; i<botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-11-23T00:00:00");
+const tempoObjetivo2 = new Date("2024-04-23T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-13T00:00:00");
 const tempoObjetivo4 = new Date("2024-12-31T00:00:00");
 
